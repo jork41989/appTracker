fab7a3b2f20dca7855bf63cfe4a5bd5f45a171ce
Jordan Ackerman <jork41989@gmail.com>
2020-02-02 18:18:46 -0500

    working on the util and reducer for jobs

--- frontend/src/actions/jobs_actions.js
+++ frontend/src/actions/jobs_actions.js
@@ -3,5 +3,5 @@ import * as JobsApiUtil from '../util/jobs_api_util'
 export const RECEIVE_JOB = "RECEIVE_JOB";
 
 export const writeJob = (job, jobId, userId) => dispatch => {
-  
+  return JobsApiUtil.addJob(job, jobId, userId)
 }
--- frontend/src/util/jobs_api_util.js
+++ frontend/src/util/jobs_api_util.js
@@ -3,3 +3,4 @@ import axios from 'axios';
 export const addJob = (data, jobId, userId) => {
   return axios.post(`/api/jobs/${jobId}/${userId}`, data)
 }
+
