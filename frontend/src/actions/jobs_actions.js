a5ab5e57364632d2d1d3d03fe9458b8f0e925f62
Jordan Ackerman <jork41989@gmail.com>
2020-02-05 21:42:59 -0500

    makinbg progress

--- frontend/src/actions/jobs_actions.js
+++ frontend/src/actions/jobs_actions.js
@@ -1,7 +1,22 @@
 import * as JobsApiUtil from '../util/jobs_api_util'
 
 export const RECEIVE_JOB = "RECEIVE_JOB";
+export const RECEIVE_JOB_ERRORS = "RECEIVE_JOB_ERRORS";
+
+const receiveJob = job => {
+  return ({
+    type: RECEIVE_JOB,
+    job
+  })
+}
+
+const receiveJobErrors = errors => ({
+  type: RECEIVE_JOB_ERRORS,
+  errors
+});
 
 export const writeJob = (job, jobId, userId) => dispatch => {
   return JobsApiUtil.addJob(job, jobId, userId)
+    .then(job => dispatch(receiveJob(job.data)),
+      err => dispatch(receiveJobErrors(err.response.data)))
 }
--- frontend/src/util/jobs_api_util.js
+++ frontend/src/util/jobs_api_util.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+
+
+
 export const addJob = (data, jobId, userId) => {
   return axios.post(`/api/jobs/${jobId}/${userId}`, data)
 }
