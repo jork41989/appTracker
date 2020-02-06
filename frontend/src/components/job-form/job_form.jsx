27b3b0511dabd1b8dc25d5f024a469894ff8f7db
Jordan Ackerman <jork41989@gmail.com>
2020-02-06 01:08:18 -0500

    more work on jobs component

---
+++ frontend/src/components/job-form/job_form.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactTooltip from 'react-tooltip';
+import { merge } from 'lodash';
+
+
+class JobForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
+
+export default JobsForm
